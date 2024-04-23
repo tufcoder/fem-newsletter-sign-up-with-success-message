c979ca8e1d63f97bac70fa8472bd5e50b05ffa08
Oswaldo Castro <oswaldogpc@gmail.com>
2024-04-23 20:11:29 -0300

    finished mobile version

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+const first = document.querySelector('.first')
+const success = document.querySelector('.success')
+const form = document.querySelector('.form')
+const btnDismiss = document.getElementById('button-dismiss')
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const emailRegExp =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    const email = document.querySelector('input[type="email"]')
+    const emailError = document.getElementById('email-error')
+    const emailConfirm = document.getElementById('email-confirm')
+
+    if (!emailRegExp.test(email.value)) {
+        if (emailError.classList.contains('hidden'))
+            emailError.classList.remove('hidden')
+
+        if (!email.classList.contains('error'))
+            email.classList.add('error')
+    } else {
+        if (email.classList.contains('error'))
+            email.classList.remove('error')
+
+        if (!emailError.classList.contains('hidden'))
+            emailError.classList.add('hidden')
+
+        first.classList.add('hidden')
+        success.classList.remove('hidden')
+        emailConfirm.textContent = email.value
+        email.value = ''
+    }
+}
+
+const handleDismiss = () => {
+    first.classList.remove('hidden')
+    success.classList.add('hidden')
+}
+
+form.addEventListener('submit', handleSubmit)
+btnDismiss.addEventListener('click', handleDismiss)
